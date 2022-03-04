7d035d8502a019bb3c28e9d88ab30b9add4eda48
magesh <hariarunengg@gmail.com>
2022-03-04 13:29:02 +0530

    side bar added and working on it 😀

---
+++ components/Sidebar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const styles = {
+  wrapper: 'bg-[#151620]',
+}
+export const Sidebar = () => {
+  return <div className={styles.wrapper}>Sidebar</div>
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,16 @@
 import Head from 'next/head'
+import SideBar, { Sidebar } from '../components/Sidebar'
+
 const Home = () => {
   return (
-    <div className="flex h-screen w-full bg-[#212030]">
+    <div className="flex  h-screen w-full justify-center  bg-[#212030]">
       <Head>
         <title>My MarketPlace</title>
         <link rel="icon" href="favicon.ico" />
       </Head>
+      <div className=" w-full justify-evenly   lg:w-[80%] lg:max-w-[1900px] lg:justify-between">
+        <Sidebar />
+      </div>
     </div>
   )
 }
