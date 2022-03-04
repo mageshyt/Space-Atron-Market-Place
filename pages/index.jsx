34f624db94aac3c20801e1f5ee75d67334d138d4
magesh <hariarunengg@gmail.com>
2022-03-04 13:09:02 +0530

    first commit 😀

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+const Home = () => {
+  return (
+    <div className="flex h-screen w-full bg-[#212030]">
+      <Head>
+        <title>My MarketPlace</title>
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+    </div>
+  )
+}
+
+export default Home
